b44b9203ef405d7688d6b3027ce278ec4e1bb8af
caiicar293 <124074010+caiicar293@users.noreply.github.com>
2023-02-17 13:48:38 -0500

    Update amb_light_service.c

--- onboarding/source/amb_light_service.c
+++ onboarding/source/amb_light_service.c
@@ -1,28 +1,20 @@
 #include "amb_light_service.h"
 #include "serial_io.h"
-#include "obc_errors.h"
+
 
 #include <adc.h>
 #include <sci.h>
 
 /* USER CODE BEGIN */
-// Include any additional headers here
+#include <stdio.h>
+#include <FreeRTOS.h>
+#include <os_task.h>
+#include <os_queue.h>
+#include <adc.h>
+#include <sci.h>
 
-/* USER CODE END */
-
-/* Light service task config */
-#define LIGHT_SERVICE_NAME "light_service"
-#define LIGHT_SERVICE_STACK_SIZE 256UL
-#define LIGHT_SERVICE_PRIORITY 1UL
-
-/* USER CODE BEGIN */
-// Define light service queue config here
-
-/* USER CODE END */
-
-/* USER CODE BEGIN */
-// Declare any global variables here
-
+static TaskHandle_t lightServiceHandle = NULL;
+static QueueHandle_t lightserviceQueue;
 /* USER CODE END */
 
 /**
@@ -31,23 +23,65 @@
  */
 static void lightServiceTask(void * pvParameters);
 
-obc_error_code_t initLightService(void) {
+uint8_t initLightService(void) {
     /* USER CODE BEGIN */
-    // Create the task and queue here. Return error code if task/queue was not created successfully.
-
+    BaseType_t xReturned = pdFAIL;
+        if (lightServiceHandle == NULL) {
+            xReturned = xTaskCreate(lightServiceTask,
+                                    LIGHT_SERVICE_NAME,
+                                    LIGHT_SERVICE_STACK_SIZE,
+                                    NULL,
+                                    LIGHT_SERVICE_PRIORITY,
+                                    &lightServiceHandle);
+    
     /* USER CODE END */
+        }
+        if (xReturned == pdFAIL) {
+             printf("\nlightServiceTask not created...\n");
+        
+        }
+
+        if (lightserviceQueue == NULL) {
+                lightserviceQueue = xQueueCreate(LIGHT_SERVICE_SIZE, LIGHT_QUEUE_SIZE);
+
+                if (lightserviceQueue == NULL) {
+                     sciPrintText(scilinREG, (unsigned char *) ERROR_MESSAGE, sizeof(ERROR_MESSAGE));
+                }
+        }
+    return 1;
 }
 
 static void lightServiceTask(void * pvParameters) {
     /* USER CODE BEGIN */
     // Wait for MEASURE_LIGHT event in the queue and then print the ambient light value to the serial port.
+    adcData_t adcData;
+    light_event_t eventReceived;
 
+
+ 	while (1) {
+        if (xQueueReceive(lightserviceQueue, &eventReceived, portMAX_DELAY) == pdPASS) {
+
+            if (eventReceived == MEASURE_LIGHT) {
+                adcStartConversion(adcREG1, adcGROUP1);
+                while (!adcIsConversionComplete(adcREG1, adcGROUP1));
+                adcGetData(adcREG1, adcGROUP1, &adcData);
+
+                char txtBuffer[TEXT_SIZE];
+                int count = snprintf(txtBuffer, TEXT_SIZE, "%u\r\n", adcData.value); 
+                sciPrintText(scilinREG, (unsigned char*)txtBuffer, count);
+            }
+
+        }
+    }
     /* USER CODE END */
 }
 
-obc_error_code_t sendToLightServiceQueue(light_event_t *event) {
+uint8_t sendToLightServiceQueue(light_event_t *event) {
     /* USER CODE BEGIN */
-    // Send the event to the queue. Return error code if event was not sent successfully.
+     xQueueSend(lightserviceQueue, event, portMAX_DELAY);
+
+    
     
     /* USER CODE END */
+    return 0;
 }
